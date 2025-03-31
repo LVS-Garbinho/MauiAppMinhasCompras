4ba92f9a621eb6a3de565cec61e9a4073c60abac
LVS-Garbinho <lucasvalsiq.1203@gmail.com>
2025-03-31 09:49:48 -0300

    Finalização do básico do aplicativo - remover e selecionar itens da lista

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -20,7 +20,7 @@ namespace MauiAppMinhasCompras.Helpers
 
         public Task<List<Produto>> Update(Produto p) 
         {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade =?, Preco =?, WHERE Id=?";
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
 
             return _conn.QueryAsync<Produto>(
                 sql, p.Descricao, p.Quantidade, p.Preco, p.Id
@@ -32,14 +32,14 @@ namespace MauiAppMinhasCompras.Helpers
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
 
-        public Task<List<Produto>> GetAll() 
+        public Task<List<Produto>> GetAll()     
         {
             return _conn.Table<Produto>().ToListAsync();
         }
 
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
